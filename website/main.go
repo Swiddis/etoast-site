6144b6c628e1c5cdbca6c9a313e930740801c286
Simeon Widdis <sawiddis@gmail.com>
2022-08-08 20:20:26 +0200

    Add initial static website

---
+++ website/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.SetTrustedProxies([]string{})
+	router.Static("/", "./static")
+	router.Run(":8080")
+}
