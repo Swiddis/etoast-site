7da7a19ab22d041fb0a537263ca7a8da15e50ec5
Simeon Widdis <sawiddis@gmail.com>
2022-08-12 21:32:58 -0600

    Configure secure web headers

--- website/main.go
+++ website/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/secure"
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
 	router := gin.Default()
+	router.Use(secure.New(secure.DefaultConfig()))
 	router.SetTrustedProxies([]string{})
 	router.Static("/", "./static")
 	err := http.Serve(autocert.NewListener("etoast.me"), router)
