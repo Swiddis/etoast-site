b0c4acaec5c5ee5ce24da99027876ee353865127
Simeon Widdis <sawiddis@gmail.com>
2022-08-14 07:02:04 +0200

    Tweaks for HTTPS

--- website/main.go
+++ website/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"github.com/gin-contrib/secure"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	secureConfig := secure.DefaultConfig()
-	secureConfig.ContentSecurityPolicy = "default-src 'self' etoast.me; style-src cdn.simplecss.org;"
-	router.Use(secure.New(secureConfig))
 	router.SetTrustedProxies([]string{})
 	router.Static("/", "./static")
 	router.Run()
