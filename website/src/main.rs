499db961755e3b1536498a9b4a79eccfc817497b
Simeon Widdis <sawiddis@gmail.com>
2023-03-27 23:39:48 -0700

    Write more thorough testing

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
 use pulldown_cmark::{html, Parser};
-use rocket::{fs::FileServer, response::Redirect};
+use rocket::{fs::FileServer, response::Redirect, Build, Rocket};
 use rocket_dyn_templates::Template;
 use serde_json::json;
 use std::{fs, path::PathBuf};
@@ -51,20 +51,50 @@ fn markdown(file: PathBuf) -> Option<Template> {
     Some(Template::render("template", context))
 }
 
-#[rocket::main]
-async fn main() {
+#[launch]
+fn rocket() -> Rocket<Build> {
     rocket::build()
         .mount("/assets", FileServer::from("static/assets").rank(-10))
         .mount("/", routes![index, favicon, markdown])
         .attach(Template::fairing())
-        .launch()
-        .await
-        .unwrap();
 }
 
 #[cfg(test)]
 mod test {
+    use super::rocket;
     use crate::make_title;
+    use rocket::{http::Status, local::blocking::Client};
+
+    #[test]
+    fn test_index_redirect() {
+        let client = Client::tracked(rocket()).unwrap();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::SeeOther);
+        assert_eq!(response.headers().get_one("Location"), Some("/home"));
+    }
+
+    #[test]
+    fn test_favicon_redirect() {
+        let client = Client::tracked(rocket()).unwrap();
+        let response = client.get("/favicon.ico").dispatch();
+        assert_eq!(response.status(), Status::SeeOther);
+        assert_eq!(response.headers().get_one("Location"), Some("/assets/favicon.ico"));
+    }
+
+    #[test]
+    fn test_markdown_template() {
+        let client = Client::tracked(rocket()).unwrap();
+        let response = client.get("/about").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert!(response.into_string().unwrap().contains("<h1>About Me</h1>"));
+    }
+
+    #[test]
+    fn test_invalid_markdown_path() {
+        let client = Client::tracked(rocket()).unwrap();
+        let response = client.get("/invalid/path").dispatch();
+        assert_eq!(response.status(), Status::NotFound);
+    }
 
     #[test]
     fn test_make_title() {
