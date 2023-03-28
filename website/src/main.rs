9c3e5613712c3295b6fd76e1f5fd942d1834c251
Simeon Widdis <sawiddis@gmail.com>
2023-03-27 18:08:35 -0700

    Add better title handling

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,18 +1,31 @@
-#[macro_use] extern crate rocket;
-use rocket::{response::Redirect, fs::FileServer};
-use rocket_dyn_templates::Template;
+#[macro_use]
+extern crate rocket;
 use pulldown_cmark::{html, Parser};
-use std::{fs, path::PathBuf};
+use rocket::{fs::FileServer, response::Redirect};
+use rocket_dyn_templates::Template;
 use serde_json::json;
+use std::{fs, path::PathBuf};
 
 fn format_markdown(markdown: &str) -> String {
     let sections = markdown.split("---");
-    sections.into_iter().map(|section| {
-        let parser = Parser::new(&section);
-        let mut section_html = String::new();
-        html::push_html(&mut section_html, parser);
-        format!("<section>{}</section>", section_html)
-    }).collect()
+    sections
+        .into_iter()
+        .map(|section| {
+            let parser = Parser::new(section);
+            let mut section_html = String::new();
+            html::push_html(&mut section_html, parser);
+            format!("<section>{}</section>", section_html)
+        })
+        .collect()
+}
+
+fn make_title(path: &str) -> String {
+    let words = path.split('/').last().unwrap_or_default().split('-');
+    let result = words.collect::<Vec<&str>>().join(" ");
+    match result.as_str() {
+        "" => "page".to_owned(),
+        _ => result.trim().to_owned()
+    }
 }
 
 #[get("/")]
@@ -30,11 +43,12 @@ fn markdown(file: PathBuf) -> Option<Template> {
     let path = format!("static/{}.md", file.as_path().display());
     let markdown = fs::read_to_string(path).ok()?;
     let html = format_markdown(&markdown);
+    let title = make_title(&file.as_path().display().to_string());
     let context = json!({
-        "title": file.as_path().display().to_string(),
+        "title": title,
         "content": html,
     });
-    Some(Template::render("template", &context))
+    Some(Template::render("template", context))
 }
 
 #[rocket::main]
@@ -46,4 +60,18 @@ async fn main() {
         .launch()
         .await
         .unwrap();
+}
+
+#[cfg(test)]
+mod test {
+    use crate::make_title;
+
+    #[test]
+    fn test_make_title() {
+        assert_eq!(make_title("/static/abc"), "abc");
+        assert_eq!(make_title("/static/"), "page");
+        assert_eq!(make_title("/static/abc-123"), "abc 123");
+        assert_eq!(make_title("/static/abc-"), "abc");
+        assert_eq!(make_title("/static/-123"), "123");
+    }
 }
