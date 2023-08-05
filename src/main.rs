42af6f94f9c18a13b1bb6d7ca50a31237e04fed8
Simeon Widdis <sawiddis@gmail.com>
2023-08-04 23:44:31 -0700

    Reimplement home redirect

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 
 use pulldown_cmark::{html, Parser};
 use rocket::fs::NamedFile;
+use rocket::response::Redirect;
 use rocket::response::content::RawHtml;
 
 #[macro_use]
 extern crate rocket;
 
-// Function to convert Markdown to HTML
 fn markdown_to_html(input: &str) -> String {
     let parser = Parser::new(input);
     let mut html_output = String::new();
@@ -15,7 +15,6 @@ fn markdown_to_html(input: &str) -> String {
     html_output
 }
 
-// Rocket route to serve Markdown files as HTML
 #[get("/<file..>")]
 fn serve_markdown(file: std::path::PathBuf) -> Option<RawHtml<String>> {
     let file_path = format!("./content/{}.md", file.display());
@@ -27,17 +26,21 @@ fn serve_markdown(file: std::path::PathBuf) -> Option<RawHtml<String>> {
     }
 }
 
-// Rocket route to serve static files (e.g., CSS, images, etc.)
 #[get("/static/<file..>")]
 async fn serve_static(file: std::path::PathBuf) -> Option<NamedFile> {
     let file_path = format!("./static/{}", file.display());
     NamedFile::open(file_path).await.ok()
 }
 
+#[get("/")]
+async fn serve_home() -> Redirect {
+    Redirect::to("/home")
+}
+
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     rocket::build()
-        .mount("/", routes![serve_markdown, serve_static])
+        .mount("/", routes![serve_markdown, serve_static, serve_home])
         .launch()
         .await?;
     Ok(())
