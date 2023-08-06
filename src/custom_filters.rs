706054674cc3343374c79296288398aefbf211bc
Simeon Widdis <sawiddis@gmail.com>
2023-08-05 17:24:10 -0700

    Begin full conversion to Tera

---
+++ src/custom_filters.rs
@@ -0,0 +1,12 @@
+use std::collections::HashMap;
+use pulldown_cmark::{html, Parser};
+use rocket_dyn_templates::tera::*;
+
+pub fn markdown(value: &Value, _: &HashMap<String, Value>) -> Result<Value> {
+    let text = try_get_value!("markdown", "value", String, value);
+
+    let parser = Parser::new(&text);
+    let mut html_output = String::new();
+    html::push_html(&mut html_output, parser);
+    Ok(to_value(html_output)?)
+}
