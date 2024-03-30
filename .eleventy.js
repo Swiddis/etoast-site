ba60cc6709e011cdba2be1386d8b2fc83e5bbed7
Simeon Widdis <sawiddis@gmail.com>
2024-03-30 01:26:20 -0700

    Add support for syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
-const mathjaxPlugin = require("eleventy-plugin-mathjax");
+const mathjax = require("eleventy-plugin-mathjax");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     static: "static",
-    "static/simple.css": "simple.css",
     "static/favicon.ico": "favicon.ico",
   });
 
-  eleventyConfig.addPlugin(mathjaxPlugin);
+  eleventyConfig.addPlugin(mathjax);
+  eleventyConfig.addPlugin(syntaxHighlight);
 
   return {
     dir: {
