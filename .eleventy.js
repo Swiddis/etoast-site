baab967c80b161d474a73329b48fec80b8db747e
Simeon Widdis <sawiddis@gmail.com>
2024-03-30 01:08:48 -0700

    Add support for mathjax rendering

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
+const mathjaxPlugin = require("eleventy-plugin-mathjax");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     static: "static",
     "static/simple.css": "simple.css",
     "static/favicon.ico": "favicon.ico",
   });
+
+  eleventyConfig.addPlugin(mathjaxPlugin);
+
   return {
     dir: {
       input: "pages",
