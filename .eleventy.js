2be4ec358d5f46ad63696add5fa524eed55d1f0c
Simeon Widdis <sawiddis@gmail.com>
2024-04-26 22:23:33 -0700

    Add footnote support to eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,16 @@
 const mathjax = require("eleventy-plugin-mathjax");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+const markdownIt = require('markdown-it');
+const markdownItFootnote = require('markdown-it-footnote');
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     static: "static",
     "static/favicon.ico": "favicon.ico",
   });
+
+  eleventyConfig.setLibrary('md', markdownIt().use(markdownItFootnote));
 
   eleventyConfig.addPlugin(mathjax);
   eleventyConfig.addPlugin(syntaxHighlight);
